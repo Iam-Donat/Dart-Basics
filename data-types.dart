cdad02dcb07200f17ac3561eedfc005aa89a744d
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-10-31 18:23:49 +0200

    some changes

---
+++ data-types.dart
@@ -0,0 +1,7 @@
+void main() {
+  String person = "John Rambo";
+
+  int age = 54;
+
+  print('The $person is now $age');
+}
--- first.dart
+++ first.dart
@@ -1,8 +1,4 @@
 void main() {
   print('Hello World');
   print('Kilifi and GOA');
-
-  print('My name is Ntwali');
-  print('Hello My name is You');
-  print('Whats up ');
 }
