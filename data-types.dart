777cf273b11c831c04d7230ce1a1bcc6c86a8e9c
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-10-31 18:26:18 +0200

    some changes

--- data-types.dart
+++ data-types.dart
@@ -1,7 +1,9 @@
 void main() {
-  String person = "John Rambo";
+  String person = 'John Rambo';
 
   int age = 54;
 
   print('The $person is now $age');
+
+  print(person.length); //getting the length of characters in a variable.
 }
