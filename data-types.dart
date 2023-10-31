06201ee4da5a160b19f9546bf9244cbf533237fd
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-10-31 18:49:01 +0200

    some changes

--- data-types.dart
+++ data-types.dart
@@ -3,7 +3,10 @@ void main() {
 
   int age = 54;
 
-  print('The $person is now $age');
+  print('The $person is now $age'); // concatination of strings.
 
   print(person.length); //getting the length of characters in a variable.
+
+  print(person
+      .toUpperCase()); //uppercase method to turn all characters in UPPERCASE
 }
---
+++ mathIn.dart
@@ -0,0 +1,13 @@
+void main() {
+  double number = 2.5;
+
+  int num = 2;
+
+  int currentYear = 20;
+
+  double result = number * currentYear;
+  int result2 = num * currentYear;
+
+  print(result);
+  print(result2);
+}
