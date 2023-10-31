52bdd2e2e0a6116feaeca72dc072c8c384cda455
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-10-31 19:00:20 +0200

    some changes

---
+++ listIn.dart
@@ -0,0 +1,13 @@
+void main() {
+  List favMovies = ['Avengers', 'Titanic', 'battleship'];
+
+  print(favMovies);
+
+  //to add a value in the list
+  favMovies.add('Aqua man');
+
+  print(favMovies);
+
+  favMovies.remove('Avengers');
+  print(favMovies);
+}
