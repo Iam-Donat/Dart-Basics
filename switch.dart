59550222a6ade092b356d305856fa14573338080
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-11-14 18:27:06 +0200

    some changes

---
+++ switch.dart
@@ -0,0 +1,34 @@
+void main() {
+  int age = 20;
+
+  //switch case
+  switch (age) {
+    case 20:
+      print('20 years old');
+      break;
+    case 30:
+      print('30 years old');
+      break;
+    case 40:
+      print('40 years old');
+      break;
+    case 50:
+      print('50 years old');
+      break;
+    case 60:
+      print('60 years old');
+      break;
+    case 70:
+      print('70 years old');
+      break;
+    case 80:
+      print('80 years old');
+      break;
+    case 90:
+      print('90 years old');
+      break;
+    case 100:
+      print('100 years old');
+      break;
+  }
+}
