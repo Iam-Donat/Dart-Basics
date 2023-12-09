97062fe0b9feaa7ac8bc816948c2142fb640a571
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-12-09 10:40:16 +0200

    some changes

---
+++ data_types.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Numbers
+  int age = 25; // int declaration is explicitly for integer numbers values
+  double pi = 3.14159; // double declaration is explicitly for decimal values
+  num zipcode = 255; // num can take both an integer and a double value
+  print(age);
+  print(pi);
+  print(zipcode);
+
+  // String interpolation
+  String name = 'Paul';
+
+  print('my name is $name'); //
+}
