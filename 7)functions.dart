ce4183992dca6572993cb988c03bfb83b1f95a84
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-12-09 21:10:03 +0200

    some changes

--- 7)functions.dart
+++ 7)functions.dart
@@ -8,6 +8,7 @@ void main() {
       height: 5,
       width:
           7)); // calling the fn without arguments won't affect the function and position in which the parameter are passed in can be altered
+  print(multiply(x: 5, y: 5)); // calling the named required fn with arguments
 }
 
 // positional options
@@ -16,3 +17,6 @@ add(int x, [int y = 0]) => x + y; //any parameter in brackets will optional
 // Named optional
 calculateArea({double width = 0.0, double height = 0.0}) =>
     width * height; //any parameter in brackets will optional by name
+multiply({required int x, required int y}) =>
+    x *
+    y;// if the want to make parameters required, the keyword required is used  before each parameter initialization but without explicitly specifying the value
