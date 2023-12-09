12f85386e3d33eec0b30d70e450ca2d1d6e9968a
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-12-09 10:54:59 +0200

    some changes

--- data_types.dart
+++ data_types.dart
@@ -1,3 +1,7 @@
+/*
+  Section 1: In this section i've learned about Datatype in Dart Programming Language.
+*/
+
 void main() {
   // Numbers
   int age = 25; // int declaration is explicitly for integer numbers values
@@ -10,5 +14,18 @@ void main() {
   // String interpolation
   String name = 'Paul';
 
-  print('my name is $name'); //
+  print(
+      'my name is $name'); //curly brace can only used if the variable name is used with something else like a method
+
+  // Boolean
+  print(age == 18); // values that return True or False
+
+  /*
+  🚧 sometime we may declare a variable without specifying the data type explicitly.
+  In this case the *var* keyword is used. Dart is capable of determining the type of the variable.
+    */
+
+  var programLanguage = 'Dart';
+  // the runtime type method is used to identify the type of the variable declared using the var keyword.
+  print(programLanguage.runtimeType);
 }
